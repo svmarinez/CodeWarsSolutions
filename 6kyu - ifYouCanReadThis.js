2f2f36a1a5dcc1c8c0e835ab5304abd876cee5f4
Sara Vasquez <30931982+svmarinez@users.noreply.github.com>
2022-05-11 19:38:32 +0200

    Create 6kyu - ifYouCanReadThis.js

---
+++ 6kyu - ifYouCanReadThis.js
@@ -0,0 +1,26 @@
+/*
+Kata Description
+
+Task
+You'll have to translate a string to Pilot's alphabet (NATO phonetic alphabet).
+
+Input:
+
+If, you can read?
+
+Output:
+
+India Foxtrot , Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta ?
+
+Note:
+
+There are preloaded dictionary you can use, named NATO
+The set of used punctuation is ,.!?.
+Punctuation should be kept in your return string, but spaces should not.
+Xray should not have a dash within.
+Every word and punctuation mark should be seperated by a space ' '.
+There should be no trailing whitespace
+
+*/
+
+const to_nato = (words) => words.replace(/\s/g, '').toLowerCase().split('').map(e => NATO[e] ? NATO[e] : e).join(' ')
