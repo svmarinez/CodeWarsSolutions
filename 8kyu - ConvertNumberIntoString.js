9f91d8b8aa6f89f279bb2f8a87efaaaa08c72de7
Sara Vasquez <30931982+svmarinez@users.noreply.github.com>
2022-05-11 19:43:43 +0200

    Create 8kyu - ConvertNumberIntoString.js

---
+++ 8kyu - ConvertNumberIntoString.js
@@ -0,0 +1,10 @@
+/*
+Kata Description
+
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+*/
+
+const numberToString = (num) => num.toString()
