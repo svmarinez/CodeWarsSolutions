4fab7a55034f0ae31bed004424845df152097398
Sara Vasquez <30931982+svmarinez@users.noreply.github.com>
2022-05-18 15:57:28 +0200

    Create 7kyu - arrayArrayArray.js

---
+++ 7kyu - arrayArrayArray.js
@@ -0,0 +1,33 @@
+/*
+Kata Description
+
+You are given an initial 2-value array (x). You will use this to calculate a score.
+
+If both values in (x) are numbers, the score is the sum of the two. If only one is a number, the score is that number. If neither is a number, return 'Void!'.
+
+Once you have your score, you must return an array of arrays. Each sub array will be the same as (x) and the number of sub arrays should be equal to the score.
+
+For example:
+
+if (x) == ['a', 3] you should return [['a', 3], ['a', 3], ['a', 3]].
+*/
+
+function explode(x){
+  let outerArr =[]
+  let score;
+    if(typeof x[0] === "number" && typeof x[1] === "number"){
+      score = x[0] + x[1]
+    } else if(typeof x[0] === "number" || typeof x[1] === "number"){
+      if(typeof x[0] === "number") {
+        score = x[0]
+      } else {
+        score = x[1]
+      }
+    } else {
+      return "Void!"
+    }
+  for(let i = 0; i < score; i++){
+    outerArr.push(x);
+  }
+  return outerArr
+}
