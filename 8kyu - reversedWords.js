89f0f52a7ef2fa26a3fe088eeda3572abc4f869f
Sara Vasquez <30931982+svmarinez@users.noreply.github.com>
2022-05-26 15:27:43 +0200

    Create 8kyu - reversedWords.js

---
+++ 8kyu - reversedWords.js
@@ -0,0 +1,7 @@
+/*
+Kata Description
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+*/
+
+const reverseWords = str => str.split(' ').reverse().join(' ')
