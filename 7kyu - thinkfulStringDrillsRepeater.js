5a9ddd0b9414080424594dbec06d98285b2b7e43
Sara Vasquez <30931982+svmarinez@users.noreply.github.com>
2022-05-26 16:22:51 +0200

    Create 7kyu - thinkfulStringDrillsRepeater.js

---
+++ 7kyu - thinkfulStringDrillsRepeater.js
@@ -0,0 +1,10 @@
+/*
+Kata Description
+
+Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+
+Example: (Input1, Input2 --> Output)
+"a", 5 --> "aaaaa"
+*/
+
+const repeater = (string, n) => string.repeat(n);
